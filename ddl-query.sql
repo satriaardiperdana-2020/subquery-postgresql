113ddb701a587a63381482f7a75518c665017285
Satria Ardi Perdana <55689358+satriaardiperdana-2020@users.noreply.github.com>
2024-05-12 12:45:28 +0700

    Update ddl-query.sql

--- ddl-query.sql
+++ ddl-query.sql
@@ -1,1 +1,36 @@
+CREATE TABLE pegawai2 (
+  kd_pegawai bigint NOT NULL,
+  nm_pegawai varchar(100) NOT NULL,
+  alamat varchar(100) NOT NULL,
+  salary bigint NOT NULL
+);
 
+
+CREATE TABLE pegawai2 (
+  kd_pegawai bigint NOT NULL,
+  nm_pegawai varchar(100) NOT NULL,
+  alamat varchar(100) NOT NULL,
+  salary bigint NOT NULL
+);
+
+CREATE TABLE pegawai3 (
+  nm_pegawai varchar(100) NOT NULL,
+  alamat varchar(100) NOT NULL
+);
+
+CREATE TABLE pegawai4 (
+  kd_pegawai bigint NOT NULL,
+  nm_pegawai varchar(100) NOT NULL,
+  alamat varchar(100) NOT NULL,
+  salary bigint NOT NULL
+) ;
+
+
+
+
+CREATE TABLE pegawai5 (
+  kd_pegawai bigint NOT NULL,
+  nm_pegawai varchar(100) NOT NULL,
+  alamat varchar(100) NOT NULL,
+  salary bigint NOT NULL
+) ;
